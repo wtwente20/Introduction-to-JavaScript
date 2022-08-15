edc25c0b6a05ae700e38fc62a0ae0a8e987c7f00
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2022-08-14 19:29:17 -0500

    Create 004.js

---
+++ basics/004.js
@@ -0,0 +1,5 @@
+console.log('5' + 10);
+
+/* The code written above will display '510' instead of 15 because
+'5' is a string and 10 is a number. When a string is paired to a number
+with the + operand, JavaScript will concat them instead of adding values.

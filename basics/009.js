7c52c260c0310894bcac3843f8ad5e2b48f4b5cc
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2022-08-14 19:58:41 -0500

    Create 009.js

---
+++ basics/009.js
@@ -0,0 +1,3 @@
+let names = ['asta', 'butterscotch', 'pudding', 'neptune', 'darwin']
+let pets = {asta: 'dog', butterscotch: 'cat', pudding: 'cat', neptune: 'fish', darwin: 'lizard'}
+console.log(pets) 

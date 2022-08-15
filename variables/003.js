398604c68ea7c1b7ad33832c95c0ac29de667007
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2022-08-14 20:13:54 -0500

    Create 003.js

---
+++ variables/003.js
@@ -0,0 +1,9 @@
+{
+  let foo = 'bar';
+}
+
+console.log(foo);
+
+/* foo is a locally declared variable, so trying to access it globally 
+is impossible because of scope. foo can only exist inside a block,
+not outside of it.

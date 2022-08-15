aa969c6f1b1216cd56187e769ff3a859d08534d5
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2022-08-14 21:21:39 -0500

    Create 004.js

---
+++ flowControl/004.js
@@ -0,0 +1,23 @@
+function barCodeScanner(serial) {
+  switch (serial) {
+    case '123':
+      console.log('Product1');
+    case '113':
+      console.log('Product2');
+    case '142':
+      console.log('Product3');
+    default:
+      console.log('Product not found!');
+  }
+}
+
+barCodeScanner('113');
+
+/* This logs:
+
+Product2
+Product3
+Product not found! */
+
+// This happens because the case is missing break 
+// statements between each case.

e61bdc5be02a4d9b9763b6bcdf4d7ac5688d77b6
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2022-08-14 19:55:26 -0500

    Create 007.js

---
+++ basics/007.js
@@ -0,0 +1,5 @@
+let foo = ['a', 'b', 'c'];
+console.log(foo.length);  // => 3
+console.log(foo[3]);      // will this result in an error?
+
+/* This code will not result in error, but as undefined instead.

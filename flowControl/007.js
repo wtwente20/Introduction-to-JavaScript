f9fdb9c1dd6abc97943acb681914f330f6d45d69
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2022-08-15 18:55:24 -0500

    Create 007.js

---
+++ flowControl/007.js
@@ -0,0 +1,10 @@
+function allCaps (string) {
+    if (string.length > 10) {
+        return string.toUpperCase();
+    } else {
+        return string;
+    }
+}
+
+console.log(allCaps('Westley Twente'));
+console.log('John Lowe')

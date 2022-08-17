98ddd3d2f84b656caf489aa1b390390aed446316
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2022-08-16 21:42:50 -0500

    Create 002.js

---
+++ loops/002.js
@@ -0,0 +1,16 @@
+function factorial(number) {
+  let result = 1;
+  for (let counter = number; counter > 0; counter -= 1) {
+    result *= counter;
+  }
+  return result;
+}
+
+console.log(factorial(1));     // => 1
+console.log(factorial(2));     // => 2
+console.log(factorial(3));     // => 6
+console.log(factorial(4));     // => 24
+console.log(factorial(5));     // => 120
+console.log(factorial(6));     // => 720
+console.log(factorial(7));     // => 5040
+console.log(factorial(8));     // => 40320

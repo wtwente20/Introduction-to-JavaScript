c4f05ee93f891b74e01fecdd13b2c6bd27ce3886
wtwente20 <100497007+wtwente20@users.noreply.github.com>
2022-08-16 21:54:01 -0500

    Create 004.js

---
+++ loops/004.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 5;) {
+  console.log(i += 1);
+}
+
+/* This will not produce an error.
+This will list:
+1
+2
+3
+4
+5
+The third component of the for statement
+is not required.
